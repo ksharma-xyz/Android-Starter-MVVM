da119ab19a3ab168d609d0819f78f265c0b894c2
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-05-25 23:12:16 +1000

    migrate from kapt to ksp (#2)

--- build-logic/convention/src/main/kotlin/xyz/ksharma/start/AndroidHiltConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/xyz/ksharma/start/AndroidHiltConventionPlugin.kt
@@ -1,10 +1,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,16 +11,12 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
 
             with(pluginManager) {
                 apply("com.google.dagger.hilt.android")
-                apply("org.jetbrains.kotlin.kapt")
+                apply("com.google.devtools.ksp")
             }
 
             dependencies {
                 "implementation"(libs.findLibrary("hilt.android").get())
-                "kapt"(libs.findLibrary("hilt.compiler").get())
-            }
-
-            configure<KaptExtension> {
-                correctErrorTypes = true
+                "ksp"(libs.findLibrary("hilt.compiler").get())
             }
         }
     }
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.hilt) apply false
-    alias (libs.plugins.kotlin.kapt) apply false
+    alias (libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
 }
 
