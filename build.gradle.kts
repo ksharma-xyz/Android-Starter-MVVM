3825f36caf67b24c168d760d87ba0e725a9011b2
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2024-07-16 22:25:08 +1000

    Update to Kotlin 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias (libs.plugins.ksp) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.detekt)
+    alias(libs.plugins.compose.compiler) apply false
 }
 
 subprojects {
